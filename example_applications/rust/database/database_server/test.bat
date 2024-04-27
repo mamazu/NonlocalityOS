135acf9ae23d83bde52e00d1d763cd837de1478c
TyRoXx <tyroxxxx@gmail.com>
2024-04-28 01:36:21 +0200

    Remove unnecessary compiler flags

--- example_applications/rust/database/database_server/test.bat
+++ example_applications/rust/database/database_server/test.bat
@@ -7,7 +7,7 @@ rem "unknown import: `env::__extenddftf2` has not been defined"
 rem This has something to do with long double (?).
 rem Solution from: https://github.com/nmandery/h3ron/blob/9d80a2bf9fd5c4f311e64ffd40087dfb41fa55a5/h3ron/examples/compile_to_wasi/Makefile
 rem https://docs.rs/cc/latest/cc/
-set CFLAGS=-pthread -DSQLITE_THREADSAFE=0 -DLONGDOUBLE_TYPE=double
+set CFLAGS=-pthread
 set RUSTFLAGS=-C target-feature=-crt-static -C link-arg=-L%wasi_compiler_unpack_dir%\wasi-sdk-22.0+m\lib\clang\18\lib\wasip1 -C link-arg=-lclang_rt.builtins-wasm32
 
 cargo build --target wasm32-wasip1-threads || exit /B 1
