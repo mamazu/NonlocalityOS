f6eb53c72282653d93ee6990f92b6a86d77ce855
TyRoXx <tyroxxxx@gmail.com>
2024-04-28 16:14:44 +0200

    Clean up

--- test.bat
+++ test.bat
@@ -12,26 +12,21 @@ set wasi_compiler_unpack_dir=%repository%\tools\%wasi_compiler_name%.0.m-mingw
 set CC_wasm32-wasi=%wasi_compiler_unpack_dir%\%wasi_compiler_name%.0+m\bin\clang.exe
 set CC_wasm32-wasip1-threads=%CC_wasm32-wasi%
 
-pushd management_service || exit /B 1
-
-rem build for the Raspberry Pi because compiling stuff on the device itself is very slow
-
-rem this target is duplicated in management_service/.cargo/config.toml
 set raspberry_pi_target=aarch64-unknown-linux-gnu
-
 rustup target add %raspberry_pi_target% || exit /B 1
 set CC_aarch64-unknown-linux-gnu=%rpi_compiler_unpack_dir%\%rpi_compiler_name%\bin\aarch64-none-linux-gnu-gcc.exe
 set AR_aarch64-unknown-linux-gnu=%rpi_compiler_unpack_dir%\%rpi_compiler_name%\bin\aarch64-none-linux-gnu-ar.exe
 set LD_LIBRARY_PATH=%rpi_compiler_unpack_dir%\%rpi_compiler_name%\aarch64-none-linux-gnu\libc\lib64
+
+pushd management_service || exit /B 1
 cargo build --target %raspberry_pi_target% --config target.aarch64-unknown-linux-gnu.linker='%CC_aarch64-unknown-linux-gnu%' --release || exit /B 1
+call .\test.bat || exit /B 1
+popd
 
 rustup toolchain install nightly-x86_64-pc-windows-msvc || exit /B 1
 
 rustup target add wasm32-wasi || exit /B 1
 rustup target add wasm32-wasip1-threads --toolchain nightly || exit /B 1
-
-call .\test.bat || exit /B 1
-popd
 
 pushd example_applications || exit /B 1
 call .\test.bat || exit /B 1
