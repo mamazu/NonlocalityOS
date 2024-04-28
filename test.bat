c61418370373046d5b1f8165807173533e2af633
TyRoXx <tyroxxxx@gmail.com>
2024-04-28 19:19:03 +0200

    test.bat clears and prints success again

--- test.bat
+++ test.bat
@@ -1,3 +1,5 @@
+@echo off
+cls || exit /B 1
 setlocal
 
 set repository=%~dp0
@@ -6,4 +8,5 @@ pushd astra || exit /B 1
 cargo run --release %repository% || exit /B 1
 popd
 
+echo Success!
 endlocal
