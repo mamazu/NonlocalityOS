85a96d0064d740699970d6e03ab66b3ee9eb7df6
TyRoXx <tyroxxxx@gmail.com>
2024-11-17 16:05:58 +0100

    jemalloc seems to improve performance

--- astraea/src/lib.rs
+++ astraea/src/lib.rs
@@ -1,4 +1,10 @@
 #![feature(test)]
+
+// seems to make the benchmarks go a bit faster than default malloc. https://crates.io/crates/jemallocator
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 pub mod compiler;
 pub mod storage;
 mod storage_benchmarks;
--- bench.sh
+++ bench.sh
@@ -4,8 +4,8 @@ set -e
 PACKAGE="dogbox_tree_editor"
 #PACKAGE="astraea"
 # cargo bench will run all benchmarks that contain any of these identifiers in their names:
-BENCHMARKS="read_large_file_sqlite_in_memory_storage"
-#BENCHMARKS="sqlite_in_memory_store_value_newly_only_refs"
+#BENCHMARKS="read_large_file_sqlite_in_memory_storage"
+BENCHMARKS=""
 
 ./install_sccache.sh || exit 1
 export RUSTC_WRAPPER=sccache
