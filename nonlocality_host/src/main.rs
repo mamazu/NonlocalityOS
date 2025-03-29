17e1e8c1d14797e4076372a428e158dc5087f878
TyRoXx <tyroxxxx@gmail.com>
2025-03-29 14:01:40 +0100

    Install systemd service

--- nonlocality_host/src/main.rs
+++ nonlocality_host/src/main.rs
@@ -1,10 +1,94 @@
-use std::path::Path;
-
-use tracing::{error, info};
+use std::{ffi::OsStr, path::Path};
+use tracing::{error, info, warn};
 use tracing_subscriber::fmt::format::FmtSpan;
+
+async fn run_process(
+    working_directory: &std::path::Path,
+    executable: &std::path::Path,
+    arguments: &[&str],
+) -> std::io::Result<()> {
+    info!("Executable: {}", executable.display());
+    info!("Arguments: {}", arguments.join(" "));
+    info!("Working directory: {}", working_directory.display());
+    let output = tokio::process::Command::new(executable)
+        .args(arguments)
+        .current_dir(&working_directory)
+        .stdin(std::process::Stdio::null())
+        .stdout(std::process::Stdio::piped())
+        .stderr(std::process::Stdio::piped())
+        .kill_on_drop(true)
+        .output()
+        .await
+        .expect("start process");
+    info!("Exit status: {}", output.status);
+    info!("Standard output:");
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    info!("{}", &stdout);
+    info!("Standard error:");
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    info!("{}", &stderr);
+    if output.status.success() {
+        Ok(())
+    } else {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("Process failed with exit code: {}", output.status),
+        ))
+    }
+}
+
+async fn install(nonlocality_directory: &Path, host_binary_name: &OsStr) -> std::io::Result<()> {
+    info!("Installing host from {}", nonlocality_directory.display());
+    let executable = &nonlocality_directory.join(host_binary_name);
+    let service_file_content = format!(
+        r#"[Unit]
+Description=NonlocalityOS Host
+After=network.target
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=15
+ExecStart={} run
+WorkingDirectory=/
+User=root
+
+[Install]
+WantedBy=multi-user.target
+"#,
+        executable.display(),
+    );
+    let temporary_directory = tempfile::tempdir().expect("create a temporary directory");
+    let service_file_name = "nonlocalityos_host.service";
+    let temporary_file_path = temporary_directory.path().join(service_file_name);
+    let mut temporary_file =
+        std::fs::File::create_new(&temporary_file_path).expect("create temporary file");
+    use std::io::Write;
+    write!(&mut temporary_file, "{}", &service_file_content)
+        .expect("write content of the service file");
+    let systemd_services_directory = std::path::Path::new("/etc/systemd/system");
+    let systemd_service_path = systemd_services_directory.join(service_file_name);
+    std::fs::rename(&temporary_file_path, &systemd_service_path)
+        .expect("move temporary service file into systemd directory");
+    let systemctl = std::path::Path::new("/usr/bin/systemctl");
+    let root_directory = std::path::Path::new("/");
+    run_process(root_directory, systemctl, &["daemon-reload"]).await?;
+    run_process(root_directory, systemctl, &["enable", service_file_name]).await?;
+    run_process(root_directory, systemctl, &["restart", service_file_name]).await?;
+    run_process(root_directory, systemctl, &["status", service_file_name]).await
+}
+
+async fn run(nonlocality_directory: &Path) -> std::io::Result<()> {
+    info!("Running host in {}", nonlocality_directory.display());
+    warn!("Nothing to do (not implemented), exiting soon.");
+    tokio::time::sleep(std::time::Duration::from_secs(15)).await;
+    warn!("Exiting");
+    Ok(())
+}
 
 async fn handle_command_line(
     nonlocality_directory: &Path,
+    host_binary_name: &OsStr,
     command_line_arguments: &[String],
 ) -> std::io::Result<()> {
     info!("Command line arguments: {:?}", command_line_arguments);
@@ -17,31 +101,13 @@ async fn handle_command_line(
     }
     let command = command_line_arguments[0].as_str();
     match command {
-        "install" => {
-            info!("Installing host in {}", nonlocality_directory.display());
-            Ok(())
-        }
-        "run" => {
-            info!("Running host in {}", nonlocality_directory.display());
-            Ok(())
-        }
+        "install" => install(nonlocality_directory, host_binary_name).await,
+        "run" => run(nonlocality_directory).await,
         _ => Err(std::io::Error::new(
             std::io::ErrorKind::InvalidInput,
             format!("Unknown command {}", command),
         )),
     }
-}
-
-#[tokio::test(flavor = "multi_thread")]
-async fn test_install_host() -> std::io::Result<()> {
-    let temporary_directory = tempfile::tempdir()?;
-    handle_command_line(temporary_directory.path(), &["install".to_string()]).await
-}
-
-#[tokio::test(flavor = "multi_thread")]
-async fn test_run_host() -> std::io::Result<()> {
-    let temporary_directory = tempfile::tempdir()?;
-    handle_command_line(temporary_directory.path(), &["run".to_string()]).await
 }
 
 #[tokio::main(flavor = "multi_thread")]
@@ -57,5 +123,11 @@ async fn main() -> std::io::Result<()> {
         "Nonlocality directory detected: {}",
         nonlocality_directory.display()
     );
-    handle_command_line(nonlocality_directory, &command_line_arguments[1..]).await
+    let host_binary_name = current_binary.file_name().unwrap();
+    handle_command_line(
+        nonlocality_directory,
+        host_binary_name,
+        &command_line_arguments[1..],
+    )
+    .await
 }
