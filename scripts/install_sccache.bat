219752701f26bf04027ebec683149402b02210e5
TyRoXx <tyroxxxx@gmail.com>
2025-03-30 17:14:04 +0200

    Upgrade sccache to 0.10.0

--- scripts/install_sccache.bat
+++ scripts/install_sccache.bat
@@ -11,13 +11,16 @@ for %%D in ("%PATH:;=" "%") do (
     if exist "%%~D\%executable%" (
         set "found=true"
         echo %executable% found in %%~D
+        echo sccache is in the PATH, so we assume that the correct version is already installed.
+        echo The reason for avoiding the cargo install command is that cargo likes to rebuild sccache unnecessarily which takes several minutes.
+        echo If you want to update sccache, run: cargo uninstall sccache
         goto :end
     )
 )
 
 if "%found%" == "false" (
     echo %executable% not found in PATH, trying to install it.
-    cargo --verbose install --locked sccache --version 0.9.1 || exit /B 1
+    cargo --verbose install --locked sccache --version 0.10.0 || exit /B 1
 )
 
 :end
--- scripts/install_sccache.sh
+++ scripts/install_sccache.sh
@@ -5,9 +5,10 @@
 if which sccache >/dev/null 2>&1; then
     echo "sccache is in the PATH, so we assume that the correct version is already installed."
     echo "The reason for avoiding the cargo install command is that cargo likes to rebuild sccache unnecessarily which takes several minutes."
+    echo "If you want to update sccache, run: cargo uninstall sccache"
     sccache --version || exit 1
 else
     echo "sccache is not in the PATH, trying to install it."
     set CARGO_LOG=info
-    cargo --verbose install --locked sccache --version 0.9.1 || exit 1
+    cargo --verbose install --locked sccache --version 0.10.0 || exit 1
 fi
