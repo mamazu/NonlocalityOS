321aa12dc959ce13c4d46fc7aeeb8a43c3a0f4d6
TyRoXx <tyroxxxx@gmail.com>
2025-04-12 10:42:13 +0200

    Move complex expression tests to lambda to prepare for a "hello world" program next

--- lambda/src/complex_expression_tests.rs
+++ lambda/src/complex_expression_tests.rs
@@ -1,28 +1,264 @@
-use crate::{FileNameObject, LoadedDirectory};
+use crate::builtins::{BUILTINS_NAMESPACE, LAMBDA_APPLY_METHOD_NAME};
+use crate::expressions::ReadLiteral;
+use crate::expressions::{
+    evaluate, Application, Expression, LambdaExpression, Object, Pointer, ReadVariable,
+};
+use crate::type_checking::TypeCheckedExpression;
+use crate::types::Name;
+use crate::types::{Interface, NamespaceId, Signature, Type, TypedExpression};
+use astraea::storage::StoreError;
 use astraea::storage::{store_object, LoadValue};
 use astraea::tree::BlobDigest;
 use astraea::{
     storage::{InMemoryValueStorage, StoreValue},
     tree::{HashedValue, Value, ValueBlob},
 };
-use dogbox_tree::serialization::FileName;
-use dogbox_tree_editor::OpenDirectory;
-use lambda::builtins::{BUILTINS_NAMESPACE, LAMBDA_APPLY_METHOD_NAME};
-use lambda::expressions::{
-    evaluate, Application, Expression, LambdaExpression, Object, Pointer, ReadVariable,
-};
-use lambda::type_checking::TypeCheckedExpression;
-use lambda::types::{Interface, Name, NamespaceId, Signature, Type, TypedExpression};
+use async_trait::async_trait;
 use std::collections::BTreeMap;
 use std::pin::Pin;
 use std::sync::Arc;
+
+#[derive(Debug)]
+pub struct FileNameObject {
+    pub content: dogbox_tree::serialization::FileName,
+}
+
+impl FileNameObject {
+    pub fn new(content: dogbox_tree::serialization::FileName) -> Self {
+        Self { content }
+    }
+}
+
+#[async_trait]
+impl Object for FileNameObject {
+    async fn call_method(
+        &self,
+        _interface: &BlobDigest,
+        _method: &Name,
+        _argument: &Pointer,
+        _storage: &(dyn LoadValue + Sync),
+        _read_variable: &Arc<ReadVariable>,
+        _read_literal: &ReadLiteral,
+    ) -> std::result::Result<Pointer, ()> {
+        todo!()
+    }
+
+    async fn serialize(
+        &self,
+        _storage: &dyn StoreValue,
+    ) -> std::result::Result<HashedValue, StoreError> {
+        todo!()
+    }
+
+    async fn serialize_to_flat_value(&self) -> Option<Arc<Value>> {
+        match Value::from_object(&self.content) {
+            Ok(success) => Some(Arc::new(success)),
+            Err(_) => todo!(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct SmallBytes {
+    single_value_content: HashedValue,
+}
+
+impl SmallBytes {
+    pub fn new(single_value_content: HashedValue) -> Self {
+        Self {
+            single_value_content,
+        }
+    }
+}
+
+#[async_trait]
+impl Object for SmallBytes {
+    async fn call_method(
+        &self,
+        _interface: &BlobDigest,
+        _method: &Name,
+        _argument: &Pointer,
+        _storage: &(dyn LoadValue + Sync),
+        _read_variable: &Arc<ReadVariable>,
+        _read_literal: &ReadLiteral,
+    ) -> std::result::Result<Pointer, ()> {
+        todo!()
+    }
+
+    async fn serialize(
+        &self,
+        _storage: &dyn StoreValue,
+    ) -> std::result::Result<HashedValue, StoreError> {
+        Ok(self.single_value_content.clone())
+    }
+
+    async fn serialize_to_flat_value(&self) -> Option<Arc<Value>> {
+        Some(self.single_value_content.value().clone())
+    }
+}
+
+#[derive(Debug)]
+pub struct LoadedFile {
+    interface: BlobDigest,
+    read: Name,
+    content: BlobDigest,
+    storage: Arc<(dyn LoadValue + Send + Sync)>,
+}
+
+impl LoadedFile {
+    pub fn new(
+        interface: BlobDigest,
+        read: Name,
+        content: BlobDigest,
+        storage: Arc<(dyn LoadValue + Send + Sync)>,
+    ) -> Self {
+        Self {
+            interface,
+            read,
+            content,
+            storage,
+        }
+    }
+}
+
+#[async_trait]
+impl Object for LoadedFile {
+    async fn call_method(
+        &self,
+        interface: &BlobDigest,
+        method: &Name,
+        _argument: &Pointer,
+        _storage: &(dyn LoadValue + Sync),
+        _read_variable: &Arc<ReadVariable>,
+        _read_literal: &ReadLiteral,
+    ) -> std::result::Result<Pointer, ()> {
+        if &self.interface == interface {
+            if &self.read == method {
+                // the argument is unit, so we don't need to check it
+                let content = self
+                    .storage
+                    .load_value(&self.content)
+                    .await.unwrap(/*TODO*/);
+                let hashed = content.hash().unwrap(/*TODO*/);
+                let result: Arc<(dyn Object + Sync)> = Arc::new(SmallBytes::new(hashed));
+                Ok(Pointer::Object(result))
+            } else {
+                todo!()
+            }
+        } else {
+            todo!()
+        }
+    }
+
+    async fn serialize(
+        &self,
+        _storage: &dyn StoreValue,
+    ) -> std::result::Result<HashedValue, StoreError> {
+        todo!()
+    }
+
+    async fn serialize_to_flat_value(&self) -> Option<Arc<Value>> {
+        None
+    }
+}
+
+#[derive(Debug)]
+pub struct LoadedDirectory {
+    value_for_references: Arc<Value>,
+    data: dogbox_tree::serialization::DirectoryTree,
+    self_interface: BlobDigest,
+    get: Name,
+    file_interface: BlobDigest,
+    read: Name,
+    storage: Arc<(dyn LoadValue + Send + Sync)>,
+}
+
+impl LoadedDirectory {
+    pub fn new(
+        value_for_references: Arc<Value>,
+        data: dogbox_tree::serialization::DirectoryTree,
+        self_interface: BlobDigest,
+        get: Name,
+        file_interface: BlobDigest,
+        read: Name,
+        storage: Arc<(dyn LoadValue + Send + Sync)>,
+    ) -> Self {
+        Self {
+            value_for_references,
+            data,
+            self_interface,
+            get,
+            file_interface,
+            read,
+            storage,
+        }
+    }
+}
+
+#[async_trait]
+impl Object for LoadedDirectory {
+    async fn call_method(
+        &self,
+        interface: &BlobDigest,
+        method: &Name,
+        argument: &Pointer,
+        _storage: &(dyn LoadValue + Sync),
+        _read_variable: &Arc<ReadVariable>,
+        _read_literal: &ReadLiteral,
+    ) -> std::result::Result<Pointer, ()> {
+        if &self.self_interface == interface {
+            if &self.get == method {
+                let argument_value = match argument.serialize_to_flat_value().await {
+                    Some(success) => success,
+                    None => todo!(),
+                };
+                let key: dogbox_tree::serialization::FileName = match argument_value.to_object() {
+                    Ok(success) => success,
+                    Err(_) => todo!(),
+                };
+                let child = match self.data.children.get(&key) {
+                    Some(found) => found,
+                    None => todo!(),
+                };
+                match &child.content {
+                    dogbox_tree::serialization::ReferenceIndexOrInlineContent::Indirect(
+                        reference_index,
+                    ) => Ok(Pointer::Object(Arc::new(LoadedFile::new(
+                        self.file_interface,
+                        self.read.clone(),
+                        self.value_for_references.references()[reference_index.0 as usize],
+                        self.storage.clone(),
+                    )))),
+                    dogbox_tree::serialization::ReferenceIndexOrInlineContent::Direct(_vec) => {
+                        todo!()
+                    }
+                }
+            } else {
+                todo!()
+            }
+        } else {
+            todo!()
+        }
+    }
+
+    async fn serialize(
+        &self,
+        _storage: &dyn StoreValue,
+    ) -> std::result::Result<HashedValue, StoreError> {
+        todo!()
+    }
+
+    async fn serialize_to_flat_value(&self) -> Option<Arc<Value>> {
+        None
+    }
+}
 
 #[tokio::test]
 async fn complex_expression() {
     let storage = Arc::new(InMemoryValueStorage::empty());
     let clock = || std::time::SystemTime::UNIX_EPOCH;
     let directory = Arc::new(
-        OpenDirectory::create_directory(storage.clone(), clock, 1)
+        dogbox_tree_editor::OpenDirectory::create_directory(storage.clone(), clock, 1)
             .await
             .unwrap(),
     );
@@ -62,7 +298,6 @@ async fn complex_expression() {
     let file_name_type = Type::Named(file_name_type_name);
     let regular_file_type_name = Name::new(namespace, "regular_file".to_string());
     let regular_file_type = Type::Named(regular_file_type_name.clone());
-    //let regular_file_type_ref = store_object(&*storage, &regular_file_type).await.unwrap();
     let get_name = Name::new(namespace, "get".to_string());
     let directory_interface = Arc::new(Interface::new(BTreeMap::from([(
         get_name.clone(),
@@ -72,7 +307,10 @@ async fn complex_expression() {
         .await
         .unwrap();
     let file_name_value = HashedValue::from(Arc::new(
-        Value::from_object(&FileName::try_from(file_name.to_string()).unwrap()).unwrap(),
+        Value::from_object(
+            &dogbox_tree::serialization::FileName::try_from(file_name.to_string()).unwrap(),
+        )
+        .unwrap(),
     ));
     let get_expression = read_lambda_parameter_expression
         .apply(
--- lambda/src/lib.rs
+++ lambda/src/lib.rs
@@ -7,3 +7,9 @@ mod expressions_tests;
 pub mod type_checking;
 mod type_checking_tests;
 pub mod types;
+
+#[cfg(test)]
+mod complex_expression_tests;
+
+#[cfg(test)]
+mod hello_world_tests;
