3c053b15766bd9dc197fce3480d96acda17ad61f
TyRoXx <tyroxxxx@gmail.com>
2025-05-01 15:51:32 +0200

    Remove unused NamespaceId::random

--- lambda/src/name.rs
+++ lambda/src/name.rs
@@ -5,12 +5,6 @@ use uuid::Uuid;
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, Serialize, Deserialize)]
 pub struct NamespaceId(pub [u8; 16]);
 
-impl NamespaceId {
-    pub fn random() -> Self {
-        Self(Uuid::new_v4().into_bytes())
-    }
-}
-
 impl Display for NamespaceId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", Uuid::from_bytes(self.0))
