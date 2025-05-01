e15d5670f2fcc7c493efdcb59470faade5ce7b61
TyRoXx <tyroxxxx@gmail.com>
2025-05-01 14:09:19 +0200

    GitHub broke the caching API

--- coverage.sh
+++ coverage.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env sh
 ./scripts/install_cargo-tarpaulin.sh || exit 1
 
+echo "Resetting sccache stats"
 sccache --zero-stats || exit 1
 
 export CARGO_TARGET_DIR=target-coverage
 export RUST_BACKTRACE=1
 # https://crates.io/crates/cargo-tarpaulin
+echo "Running cargo tarpaulin"
 cargo tarpaulin --verbose --out lcov --out html --include-tests --ignore-panics --count --output-dir target-coverage --skip-clean --engine llvm --exclude-files 'target/*' || exit 1
 
+echo "Showing sccache stats"
 sccache --show-stats || exit 1
--- scripts/install_cargo-tarpaulin.sh
+++ scripts/install_cargo-tarpaulin.sh
@@ -3,4 +3,5 @@ scripts_dir=$(dirname "$(realpath "$0")")
 $scripts_dir/install_sccache.sh || exit 1
 
 export RUSTC_WRAPPER=sccache
+echo "Installing cargo-tarpaulin"
 cargo install --version 0.31.2 --locked cargo-tarpaulin || exit 1
