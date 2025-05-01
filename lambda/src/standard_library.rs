843b9c83fac2bc1231936413b58c25d8b372f5e4
TyRoXx <tyroxxxx@gmail.com>
2025-05-01 15:32:54 +0200

    Remove unused AndThen

--- lambda/src/standard_library.rs
+++ lambda/src/standard_library.rs
@@ -22,35 +22,3 @@ impl ConsoleOutput {
         })
     }
 }
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct AndThen {
-    pub effect: BlobDigest,
-    pub handle_result: BlobDigest,
-}
-
-impl AndThen {
-    pub fn new(effect: BlobDigest, handle_result: BlobDigest) -> Self {
-        Self {
-            effect,
-            handle_result,
-        }
-    }
-
-    pub fn to_value(&self) -> Value {
-        Value::new(ValueBlob::empty(), vec![self.effect, self.handle_result])
-    }
-
-    pub fn from_value(value: &Value) -> Option<AndThen> {
-        if value.blob().len() != 0 {
-            return None;
-        }
-        if value.references().len() != 2 {
-            return None;
-        }
-        Some(AndThen {
-            effect: value.references()[0],
-            handle_result: value.references()[1],
-        })
-    }
-}
