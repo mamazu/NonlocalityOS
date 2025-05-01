e0ede0fdc1aa2100b73e4e8f19c7da940a9bedbf
TyRoXx <tyroxxxx@gmail.com>
2025-05-01 16:13:34 +0200

    Test digest calculations

--- astraea/src/tree.rs
+++ astraea/src/tree.rs
@@ -328,3 +328,115 @@ pub fn calculate_reference(referenced: &Value) -> BlobDigest {
     let result: [u8; 64] = calculate_digest_fixed::<sha3::Sha3_512>(referenced).into();
     BlobDigest::new(&result)
 }
+
+#[test]
+fn test_calculate_reference_blob_no_references_0() {
+    let value = Arc::new(Value::empty());
+    let reference = calculate_reference(&value);
+    assert_eq!(
+        reference,
+        BlobDigest::parse_hex_string(
+            "a69f73cca23a9ac5c8b567dc185a756e97c982164fe25859e0d1dcc1475c80a615b2123af1f5f94c11e3e9402c3ac558f500199d95b6d3e301758586281dcd26").unwrap());
+}
+
+#[test]
+fn test_calculate_reference_blob_yes_references_0() {
+    let value = Arc::new(Value::new(
+        ValueBlob::try_from(bytes::Bytes::from("Hello, world!")).unwrap(),
+        Vec::new(),
+    ));
+    let reference = calculate_reference(&value);
+    assert_eq!(
+        reference,
+        BlobDigest::parse_hex_string(
+            "8e47f1185ffd014d238fabd02a1a32defe698cbf38c037a90e3c0a0a32370fb52cbd641250508502295fcabcbf676c09470b27443868c8e5f70e26dc337288af").unwrap());
+}
+
+#[test]
+fn test_calculate_reference_blob_no_references_1() {
+    let value = Arc::new(Value::new(
+        ValueBlob::empty(),
+        vec![BlobDigest(([0u8; 32], [0u8; 32]))],
+    ));
+    let reference = calculate_reference(&value);
+    assert_eq!(
+        reference,
+        BlobDigest::parse_hex_string(
+            "f8d76fdd8a082a67eaab47b5518ac486cb9a90dcb9f3c9efcfd86d5c8b3f1831601d3c8435f84b9e56da91283d5b98040e6e7b2c8dd9aa5bd4ebdf1823a7cf29").unwrap());
+}
+
+#[test]
+fn test_calculate_reference_blob_yes_references_1() {
+    let value = Arc::new(Value::new(
+        ValueBlob::try_from(bytes::Bytes::from("Hello, world!")).unwrap(),
+        vec![BlobDigest(([0u8; 32], [0u8; 32]))],
+    ));
+    let reference = calculate_reference(&value);
+    assert_eq!(
+        reference,
+        BlobDigest::parse_hex_string(
+            "9bcc4c3988a38c854a4bacd9e09e438655453e12f1b5196771c52bd87e280b28012145c21a7488c05c2c7909003c1b694532b22ec38f6cb1d3b7067defe2b58d").unwrap());
+}
+
+#[test]
+fn test_calculate_reference_blob_no_references_2() {
+    let value = Arc::new(Value::new(
+        ValueBlob::empty(),
+        vec![
+            BlobDigest(([0u8; 32], [0u8; 32])),
+            BlobDigest(([1u8; 32], [1u8; 32])),
+        ],
+    ));
+    let reference = calculate_reference(&value);
+    assert_eq!(
+        reference,
+        BlobDigest::parse_hex_string(
+            "92a55955611e3ba7f935fe2e518504c7e37dfd9f9809faa1be93cd91e3c14a5914acd44ea9f442389a8ee3649de60ede71a8d9b85d3560d8ced78216597db304").unwrap());
+}
+
+#[test]
+fn test_calculate_reference_blob_yes_references_2() {
+    let value = Arc::new(Value::new(
+        ValueBlob::try_from(bytes::Bytes::from("Hello, world!")).unwrap(),
+        vec![
+            BlobDigest(([0u8; 32], [0u8; 32])),
+            BlobDigest(([1u8; 32], [1u8; 32])),
+        ],
+    ));
+    let reference = calculate_reference(&value);
+    assert_eq!(
+        reference,
+        BlobDigest::parse_hex_string(
+            "a8f64b8aabe17bc2091f8a801d67501f69c26b8a9164890c4e185a03ff82cf98c72baed1c78a4281aed776ca757cbe5d573e3874bacf8cd45cec5c3c5d0b1279").unwrap());
+}
+
+#[test]
+fn test_calculate_reference_collision() {
+    let digest = BlobDigest(([0u8; 32], [0u8; 32]));
+    let digest_in_reference = Arc::new(Value::new(ValueBlob::empty(), vec![digest.clone()]));
+    let digest_in_blob = Arc::new(Value::new(
+        ValueBlob::try_from(
+            [
+                &DEPRECATED_TYPE_ID_IN_DIGEST.to_be_bytes(),
+                &digest.0 .0[..],
+                &digest.0 .1[..],
+            ]
+            .concat()
+            .into(),
+        )
+        .unwrap(),
+        vec![],
+    ));
+    let digest_in_reference_digest = calculate_reference(&digest_in_reference);
+    assert_eq!(
+        digest_in_reference_digest,
+        BlobDigest::parse_hex_string(
+            "f8d76fdd8a082a67eaab47b5518ac486cb9a90dcb9f3c9efcfd86d5c8b3f1831601d3c8435f84b9e56da91283d5b98040e6e7b2c8dd9aa5bd4ebdf1823a7cf29").unwrap());
+    let digest_in_blob_digest = calculate_reference(&digest_in_blob);
+    assert_eq!(
+        digest_in_blob_digest,
+        BlobDigest::parse_hex_string(
+            "f8d76fdd8a082a67eaab47b5518ac486cb9a90dcb9f3c9efcfd86d5c8b3f1831601d3c8435f84b9e56da91283d5b98040e6e7b2c8dd9aa5bd4ebdf1823a7cf29").unwrap());
+    // this collision is a design error and should be corrected
+    assert_eq!(digest_in_reference_digest, digest_in_blob_digest);
+}
