1d0acf41772c2d655f39a4d43ea25421a20871e4
TyRoXx <tyroxxxx@gmail.com>
2025-09-09 13:12:30 +0200

    GH-311: Fuzz for only 10 seconds

--- fuzz.sh
+++ fuzz.sh
@@ -4,10 +4,10 @@ set -e
 ./scripts/install_cargo-fuzz.sh
 JOBS=`nproc`
 FUZZ_TARGET_DIR=./fuzz/fuzz_targets
-FUZZ_RUNS=100
+TIME_PER_TARGET=10
 
 for file in $(ls "$FUZZ_TARGET_DIR"/*.rs | sort); do
     target_name=$(basename "$file" .rs)
     echo "Running fuzz target: $target_name"
-    cargo fuzz run --release --jobs $JOBS "$target_name" -- -runs=$FUZZ_RUNS || exit 1
+    cargo fuzz run --release --jobs $JOBS "$target_name" -- -max_total_time=$TIME_PER_TARGET || exit 1
 done
