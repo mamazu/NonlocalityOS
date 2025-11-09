8fb2486328c261841746c1242ac2210bcd018545
TyRoXx <tyroxxxx@gmail.com>
2025-11-09 10:01:58 +0100

    Remove references to deleted scripts

--- test.bat
+++ test.bat
@@ -1,6 +1,3 @@
 @echo off
 call .\scripts\install_cargo-nextest.bat || exit /B 1
 cargo nextest run || exit /B 1
-
-echo Building fuzzers
-call .\scripts\build_fuzz.bat || exit /B 1
--- test.sh
+++ test.sh
@@ -3,6 +3,3 @@ set -e
 
 ./scripts/install_cargo-nextest.sh
 cargo nextest run
-
-echo 'Building fuzzers'
-./scripts/build_fuzz.sh
