02dec2e60f5f2ca0832d357b2e5452a4459aaeac
TyRoXx <tyroxxxx@gmail.com>
2025-12-25 10:19:13 +0100

    Reduce fuzzing time for GitHub because the build takes too long

--- fuzz.sh
+++ fuzz.sh
@@ -4,6 +4,6 @@ set -e
 TARGET_NAME="$1"
 ./scripts/install_cargo-fuzz.sh
 JOBS=`nproc`
-TIME_PER_TARGET=10
+TIME_PER_TARGET=7
 
 cargo fuzz run --release --jobs "$JOBS" "$TARGET_NAME" -- -max_total_time=$TIME_PER_TARGET
